82a0c61fe2e42c47b59c355e1b6ed4f2b01eb0d7
Michael Yang <mikkyangg@gmail.com>
2014-11-07 15:13:38 -0500

    Implement BlasVector on Vec and slice

--- src/vector/mod.rs
+++ src/vector/mod.rs
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
+use std::raw::Repr;
+
 pub mod ll;
 pub mod ops;
 
@@ -10,4 +12,44 @@ pub trait BlasVector<T> {
     fn len(&self) -> i32;
     fn as_ptr(&self) -> *const T;
     fn as_mut_ptr(&mut self) -> *mut T;
+}
+
+impl<T> BlasVector<T> for Vec<T> {
+    #[inline]
+    fn inc(&self) -> i32 { 1i32 }
+
+    #[inline]
+    fn len(&self) -> i32 {
+        let l: Option<i32> = NumCast::from(self.len());
+        match l {
+            Some(l) => l,
+            None => panic!(),
+        }
+    }
+
+    #[inline]
+    fn as_ptr(&self) -> *const T { self.as_slice().as_ptr() }
+
+    #[inline]
+    fn as_mut_ptr(&mut self) -> *mut T { self.as_mut_slice().as_mut_ptr() }
+}
+
+impl<'a, T> BlasVector<T> for &'a [T] {
+    #[inline]
+    fn inc(&self) -> i32 { 1i32 }
+
+    #[inline]
+    fn len(&self) -> i32 {
+        let l: Option<i32> = NumCast::from(self.len());
+        match l {
+            Some(l) => l,
+            None => panic!(),
+        }
+    }
+
+    #[inline]
+    fn as_ptr(&self) -> *const T { self.repr().data }
+
+    #[inline]
+    fn as_mut_ptr(&mut self) -> *mut T { self.repr().data as *mut T }
 }
