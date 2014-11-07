9dbc227bc42f420ded149c7f74d8c04b54bd9d80
Michael Yang <mikkyangg@gmail.com>
2014-11-07 16:34:26 -0500

    Add Default convenience trait

---
+++ src/default.rs
@@ -0,0 +1,33 @@
+// Copyright 2014 Michael Yang. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+extern crate num;
+
+use self::num::complex::Complex;
+
+pub trait Default {
+    fn one() -> Self;
+    fn zero() -> Self;
+}
+
+macro_rules! default_impl(
+    ($t:ty, $ov:expr, $zv:expr) => {
+        impl Default for $t {
+            #[inline]
+            fn one() -> $t { $ov }
+            #[inline]
+            fn zero() -> $t { $zv }
+        }
+
+        impl Default for Complex<$t> {
+            #[inline]
+            fn one() -> Complex<$t> { Complex::new($ov, $zv) }
+            #[inline]
+            fn zero() -> Complex<$t> { Complex::new($zv, $zv) }
+        }
+    }
+)
+
+default_impl!(f32, 1f32, 0f32)
+default_impl!(f64, 1f64, 0f64)
