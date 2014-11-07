17208564c75d40a4b1db00c7cd1eda3bdac3d8f8
Michael Yang <mikkyangg@gmail.com>
2014-11-07 14:33:26 -0500

    Add convenience pointer trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 // Copyright 2014 Michael Yang. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
+#![feature(macro_rules)]
 
 pub mod vector;
 pub mod matrix_vector;
 pub mod matrix;
 
 pub mod attribute;
+mod pointer;
