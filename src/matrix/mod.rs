498bbd02d6a3ea03ff37a570953ebdb732bd9625
Michael Yang <mikkyangg@gmail.com>
2014-11-07 16:57:30 -0500

    Add BlasMatrix trait

--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -2,4 +2,26 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
+use attribute::{
+    Order,
+    RowMajor,
+    Transpose,
+    NoTrans,
+};
+
 pub mod ll;
+
+pub trait BlasMatrix<T> {
+    fn lead_dim(&self) -> i32 { self.rows() }
+    fn order(&self) -> Order { RowMajor }
+    fn transpose(&self) -> Transpose { NoTrans }
+    fn rows(&self) -> i32;
+    fn cols(&self) -> i32;
+    fn as_ptr(&self) -> *const T;
+    fn as_mut_ptr(&mut self) -> *mut T;
+}
+
+pub trait BandMatrix<T>: BlasMatrix<T> {
+    fn sub_diagonals(&self) -> i32;
+    fn sup_diagonals(&self) -> i32;
+}
