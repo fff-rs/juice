0c4a31d99978cbe24a1d0734f7bafba95c1e45fc
Michael Yang <mikkyangg@gmail.com>
2014-11-07 17:43:30 -0500

    Add Symm and Hemm traits

--- src/matrix/ops.rs
+++ src/matrix/ops.rs
@@ -5,6 +5,7 @@
 extern crate num;
 
 use self::num::complex::{Complex32, Complex64};
+use attribute::{Side, Symmetry};
 use pointer::CPtr;
 use scalar::Scalar;
 use matrix;
@@ -38,3 +39,38 @@ gemm_impl!(f32,       cblas_sgemm)
 gemm_impl!(f64,       cblas_dgemm)
 gemm_impl!(Complex32, cblas_cgemm)
 gemm_impl!(Complex64, cblas_zgemm)
+
+pub trait Symm {
+    fn symm(side: Side, symmetry: Symmetry, alpha: Self, a: &BlasMatrix<Self>, b: &BlasMatrix<Self>, beta: Self, c: &mut BlasMatrix<Self>);
+}
+
+pub trait Hemm {
+    fn hemm(side: Side, symmetry: Symmetry, alpha: Self, a: &BlasMatrix<Self>, b: &BlasMatrix<Self>, beta: Self, c: &mut BlasMatrix<Self>);
+}
+
+macro_rules! symm_impl(
+    ($trait_name: ident, $fn_name: ident, $t: ty, $symm_fn: ident) => (
+        impl $trait_name for $t {
+            fn $fn_name(side: Side, symmetry: Symmetry, alpha: $t, a: &BlasMatrix<$t>, b: &BlasMatrix<$t>, beta: $t, c: &mut BlasMatrix<$t>) {
+                unsafe {
+                    matrix::ll::$symm_fn(a.order(),
+                        side, symmetry,
+                        a.rows(), b.cols(),
+                        (&alpha).as_const(),
+                        a.as_ptr().as_c_ptr(), a.lead_dim(),
+                        b.as_ptr().as_c_ptr(), b.lead_dim(),
+                        (&beta).as_const(),
+                        c.as_mut_ptr().as_c_ptr(), c.lead_dim());
+                }
+            }
+        }
+    );
+)
+
+symm_impl!(Symm, symm, f32,       cblas_ssymm)
+symm_impl!(Symm, symm, f64,       cblas_dsymm)
+symm_impl!(Symm, symm, Complex32, cblas_csymm)
+symm_impl!(Symm, symm, Complex64, cblas_zsymm)
+
+symm_impl!(Hemm, hemm, Complex32, cblas_chemm)
+symm_impl!(Hemm, hemm, Complex64, cblas_zhemm)
