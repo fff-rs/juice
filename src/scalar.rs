adf1eaf09f5658d04ed6e92b86521fa55adb5779
Michael Yang <mikkyangg@gmail.com>
2014-11-07 14:39:03 -0500

    Add scalar convenience trait

---
+++ src/scalar.rs
@@ -0,0 +1,45 @@
+// Copyright 2014 Michael Yang. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+extern crate libc;
+extern crate num;
+
+use self::num::complex::Complex;
+use self::libc::{c_double, c_float, c_void};
+
+pub trait Scalar<T, S> {
+    fn as_const(self) -> T;
+    fn as_mut(self) -> S;
+}
+
+macro_rules! scalar_impl(
+    ($t: ty, $c_type: ty) => (
+        impl<'a> Scalar<$t, *mut $t> for &'a $t {
+            #[inline]
+            fn as_const(self) -> $t {
+                *self as $c_type
+            }
+
+            #[inline]
+            fn as_mut(self) -> *mut $t {
+                &self as *const _ as *mut $c_type
+            }
+        }
+
+        impl<'a> Scalar<*const c_void, *mut c_void> for &'a Complex<$t> {
+            #[inline]
+            fn as_const(self) -> *const c_void {
+                self as *const _ as *const c_void
+            }
+
+            #[inline]
+            fn as_mut(self) -> *mut c_void {
+                self as *const _ as *mut c_void
+            }
+        }
+    );
+)
+
+scalar_impl!(f32, c_float)
+scalar_impl!(f64, c_double)
