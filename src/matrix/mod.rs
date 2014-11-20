f62dd5a918d8cd30d6322390a963d64694070dcb
Michael Yang <mikkyangg@gmail.com>
2014-11-20 01:16:16 -0500

    Update for nightly

--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -2,11 +2,10 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
+use std::num::NumCast;
 use attribute::{
     Order,
-    RowMajor,
     Transpose,
-    NoTrans,
 };
 use mat::Mat;
 
@@ -15,8 +14,8 @@ pub mod ops;
 
 pub trait Matrix<T> {
     fn lead_dim(&self) -> i32 { self.rows() }
-    fn order(&self) -> Order { RowMajor }
-    fn transpose(&self) -> Transpose { NoTrans }
+    fn order(&self) -> Order { Order::RowMajor }
+    fn transpose(&self) -> Transpose { Transpose::NoTrans }
     fn rows(&self) -> i32;
     fn cols(&self) -> i32;
     fn as_ptr(&self) -> *const T;
