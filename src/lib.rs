3789e7e422b283872cb1b116b61819783bc38598
Michael Yang <mikkyangg@gmail.com>
2014-12-22 01:57:09 -0500

    Unfeature-gate tuple indexing

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 // Copyright 2014 Michael Yang. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
-#![feature(tuple_indexing)]
 #![feature(macro_rules)]
 
 pub use vector::Vector;
