e11e4f45eec5fa6209324ae69aca80d2c0133dc9
Michael Yang <mikkyangg@gmail.com>
2014-12-31 17:50:20 +0800

    Add Copy impl to attributes

--- src/attribute.rs
+++ src/attribute.rs
@@ -3,12 +3,14 @@
 // license that can be found in the LICENSE file.
 
 #[repr(C)]
+#[deriving(Copy)]
 pub enum Order {
     RowMajor=101,
     ColMajor=102,
 }
 
 #[repr(C)]
+#[deriving(Copy)]
 pub enum Transpose {
     NoTrans=111,
     Trans=112,
@@ -16,18 +18,21 @@ pub enum Transpose {
 }
 
 #[repr(C)]
+#[deriving(Copy)]
 pub enum Symmetry {
     Upper=121,
     Lower=122,
 }
 
 #[repr(C)]
+#[deriving(Copy)]
 pub enum Diagonal {
     NonUnit=131,
     Unit=132,
 }
 
 #[repr(C)]
+#[deriving(Copy)]
 pub enum Side {
     Left=141,
     Right=142,
