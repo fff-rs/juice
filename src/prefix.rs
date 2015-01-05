99b09e8312aaefd9ebf450f6205259a86da9c917
Michael Yang <mikkyangg@gmail.com>
2015-01-05 18:18:13 +0900

    Shorten macros with common prefixes

--- src/prefix.rs
+++ src/prefix.rs
@@ -5,6 +5,8 @@
 macro_rules! prefix(
     (f32, $f: ident) => (concat_idents!(cblas_s, $f));
     (f64, $f: ident) => (concat_idents!(cblas_d, $f));
+    (Complex<f32>, $f: ident) => (concat_idents!(cblas_c, $f));
+    (Complex<f64>, $f: ident) => (concat_idents!(cblas_z, $f));
     (Complex32, $f: ident) => (concat_idents!(cblas_c, $f));
     (Complex64, $f: ident) => (concat_idents!(cblas_z, $f));
 );
