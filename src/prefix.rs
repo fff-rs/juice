02bb35a427cfeb758cb732ca6bf8c1ba114f9237
Michael Yang <mikkyangg@gmail.com>
2015-01-05 17:59:52 +0900

    Add prefix macro

---
+++ src/prefix.rs
@@ -0,0 +1,10 @@
+// Copyright 2014 Michael Yang. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+macro_rules! prefix(
+    (f32, $f: ident) => (concat_idents!(cblas_s, $f));
+    (f64, $f: ident) => (concat_idents!(cblas_d, $f));
+    (Complex32, $f: ident) => (concat_idents!(cblas_c, $f));
+    (Complex64, $f: ident) => (concat_idents!(cblas_z, $f));
+);
