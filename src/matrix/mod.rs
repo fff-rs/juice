9a714962f2c6fc146750ccd78daca8a1c6198d56
Michael Yang <mikkyangg@gmail.com>
2015-01-09 14:49:01 +0900

    Update visibility and stability

--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -7,6 +7,7 @@ use attribute::{
     Transpose,
 };
 
+#[stable]
 pub mod ll;
 pub mod ops;
 
--- src/matrix_vector/mod.rs
+++ src/matrix_vector/mod.rs
@@ -2,5 +2,6 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
+#[stable]
 pub mod ll;
 pub mod ops;
