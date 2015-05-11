91c9c057fb993497ddc473a975808951175a14d2
Michael Yang <mikkyangg@gmail.com>
2015-05-11 10:39:05 -0400

    Add documentation

--- src/attribute.rs
+++ src/attribute.rs
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
+//! Various attributes of vectors and matrices.
+
 #[repr(C)]
 #[derive(Copy, Clone)]
 pub enum Order {
