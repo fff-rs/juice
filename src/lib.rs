d2740420fc2f2a28ff953c617569886664650e21
Michael Yang <mikkyangg@gmail.com>
2015-05-11 15:02:08 -0400

    Add project documentation

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,21 @@
 #![feature(core)]
 #![feature(libc)]
 
+//! BLAS bindings and wrappers.
+//!
+//! Bindings are split by BLAS level and contained in a module named `ll`
+//! (stands for low level, not sure if that's the best name, but that's
+//! what it is).
+//!
+//! Wrappers are split likewise. They are named after the function they wrap,
+//! but capitalized and contained in their respective `ops` modules. To use
+//! these wrappers, the appropriate traits must be implemented for the type.
+//! These are either `Vector` or `Matrix`.
+//!
+//! * Level 1: `vector`
+//! * Level 2: `matrix_vector`
+//! * Level 3: `matrix`
+
 extern crate core;
 extern crate libc;
 extern crate num;
