7a7308ece787634c05bd7f9c4569d496c0172c0e
Michael Yang <mikkyangg@gmail.com>
2015-06-10 10:41:50 +0900

    Specify blas with env var

--- build.rs
+++ build.rs
@@ -1,3 +1,30 @@
+use std::env::var;
+use std::ascii::AsciiExt;
+
 fn main() {
-    println!("cargo:rustc-link-lib=dylib=blas");
+    // Format: CARGO_BLAS_IMPL = (STATIC|DYLIB)_(BLAS_NAME)
+    let lib = match var("CARGO_BLAS_IMPL") {
+        Ok(s) => s,
+        Err(_) => "DYLIB_BLAS".into(),
+    };
+
+    let mut pieces = lib.split("_");
+    let link_type = match pieces.next() {
+        Some(t) => {
+            if t == "STATIC" {
+                "static"
+            } else if t == "DYLIB" {
+                "dylib"
+            } else {
+                panic!("Link type must be either STATIC or DYLIB")
+            }
+        },
+        None => panic!("Format error"),
+    };
+    let link_name = match pieces.next() {
+        Some(t) => t.to_ascii_lowercase(),
+        None => panic!("Format error"),
+    };
+
+    println!("cargo:rustc-link-lib={}={}", link_type, link_name);
 }
