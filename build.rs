7ae568ac79c73cf450ccb664f63d10a57208ed53
Michael Yang <mikkyangg@gmail.com>
2015-06-10 11:17:22 +0900

    Simplify env vars

--- build.rs
+++ build.rs
@@ -1,30 +1,15 @@
 use std::env::var;
-use std::ascii::AsciiExt;
 
 fn main() {
-    // Format: CARGO_BLAS_IMPL = (STATIC|DYLIB)_(BLAS_NAME)
-    let lib = match var("CARGO_BLAS_IMPL") {
-        Ok(s) => s,
-        Err(_) => "DYLIB_BLAS".into(),
-    };
-
-    let mut pieces = lib.split("_");
-    let link_type = match pieces.next() {
-        Some(t) => {
-            if t == "STATIC" {
-                "static"
-            } else if t == "DYLIB" {
-                "dylib"
-            } else {
-                panic!("Link type must be either STATIC or DYLIB")
-            }
-        },
-        None => panic!("Format error"),
-    };
-    let link_name = match pieces.next() {
-        Some(t) => t.to_ascii_lowercase(),
-        None => panic!("Format error"),
-    };
+    let link_type = env_or_default("CARGO_BLAS_TYPE", "dylib");
+    let link_name = env_or_default("CARGO_BLAS", "blas");
 
     println!("cargo:rustc-link-lib={}={}", link_type, link_name);
+}
+
+fn env_or_default(var_name: &str, default: &str) -> String {
+    match var(var_name) {
+        Ok(s) => s,
+        Err(_) => default.into(),
+    }
 }
