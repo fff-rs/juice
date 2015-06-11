2aa63420244b85d04ad6464eb6403b2e63677f80
Michael Yang <mikkyangg@gmail.com>
2015-06-12 00:54:05 +0900

    Add deref to trans

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
+use std::ops::Deref;
+
 pub use self::mat::Mat;
 
 pub mod mat;
@@ -11,6 +13,17 @@ pub mod matrix_vector;
 pub enum Trans<A> {
     T(A),
     H(A),
+}
+
+impl<A> Deref for Trans<A> {
+    type Target = A;
+
+    fn deref(&self) -> &A {
+        match self {
+            &Trans::T(ref v) => v,
+            &Trans::H(ref v) => v,
+        }
+    }
 }
 
 pub enum Marker {
