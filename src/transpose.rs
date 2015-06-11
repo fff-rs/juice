420563f0eabcdd37459a3f6b94402fefe5281f8b
Michael Yang <mikkyangg@gmail.com>
2015-06-11 23:31:07 +0900

    Add new transpose type

---
+++ src/transpose.rs
@@ -0,0 +1,13 @@
+// Copyright 2015 Michael Yang. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+pub enum Trans<A> {
+    T(A),
+    H(A),
+}
+
+pub enum Marker {
+    T,
+    H,
+}
