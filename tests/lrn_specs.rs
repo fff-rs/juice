fb2862a370ad33d18099a0e017fe9bcd60cdd838
Ewan Higgs <ewan.higgs@ugent.be>
2015-12-24 00:14:53 +0100

    test/test Add function stubs for native and applicable (ignored) tests.
    A significant refactor of the code was merged recently that implemented the cuda
    back end. Native and OpenCL were left behind and commented out. Further,
    building with `cargo test --no-default-features --feature native` failed because
    cudann was still being pulled in by the tests. This commit:
    
    1. fixes the cfg guards so dropping cuda support is possible for some people.
    
    2. Adds the function stubs for the Native backend. These are left w/
    notimplemented!();
    
    3. The same tests for the cuda backend are copy/pasted and refactored to apply
    to the Native backend.
    
    This puts us in a good position for someone to just implement the Native backend
    functions now.
    
    NB: There is still a failing test: the lib.rs doc test requires Cuda. Adding a
    cfg guard to it uglifies it too much for it to remain a useful example, so I
    left it alone.

--- tests/lrn_specs.rs
+++ tests/lrn_specs.rs
@@ -2,7 +2,8 @@ extern crate collenchyma_nn as co_nn;
 extern crate collenchyma as co;
 
 #[cfg(test)]
-mod lrn_spec {
+#[cfg(feature = "cuda")]
+mod lrn_spec_cuda {
 
     use co::backend::{Backend, BackendConfig};
     use co::framework::IFramework;
@@ -214,6 +215,198 @@ mod lrn_spec {
             Ok(_) => {
                 result_diff.sync(native.device()).unwrap();
                 if let Some(mem) = result_diff.get(native.device()).unwrap().as_native() {
+                    assert_eq!(&[0.594536669478436f64, 0.594536669478436f64, 1.188672127844352f64], mem.as_slice::<f64>());
+                }
+            },
+            Err(err) => { println!("{:?}", err); assert!(false) }
+        }
+    }
+}
+
+#[cfg(test)]
+#[cfg(feature = "native")]
+mod lrn_spec_native {
+
+    use co::backend::{Backend, BackendConfig};
+    use co::framework::IFramework;
+    use co::frameworks::Native;
+    use co_nn::*;
+    use co::memory::MemoryType;
+    use co::tensor::SharedTensor;
+    use co::plugin::numeric_helpers::{cast, Float};
+
+    fn get_native_backend() -> Backend<Native> {
+        let framework = Native::new();
+        let hardwares = framework.hardwares();
+        let backend_config = BackendConfig::new(framework, hardwares);
+        Backend::new(backend_config).unwrap()
+    }
+
+    fn write_to_memory<T: Copy>(mem: &mut MemoryType, data: &[T]) {
+        let &mut MemoryType::Native(ref mut mem) = mem;
+        let mut mem_buffer = mem.as_mut_slice::<T>();
+        for (index, datum) in data.iter().enumerate() {
+            mem_buffer[index] = *datum;
+        }
+    }
+
+    fn get_memory<T: Float, B: IFramework + Clone>(backend: &Backend<B>) -> (SharedTensor<T>, SharedTensor<T>){
+        let val = cast::<f64, T>(1f64).unwrap();
+        let val2 = cast::<f64, T>(2f64).unwrap();
+        let mut x = SharedTensor::<T>::new(backend.device(), &(1, 1, 3)).unwrap();
+        write_to_memory(x.get_mut(backend.device()).unwrap(), &[val, val, val2]);
+
+        let result = SharedTensor::<T>::new(backend.device(), &(1, 1, 3)).unwrap();
+
+        (x, result)
+    }
+
+    fn get_grad_memory<T: Float, B: IFramework + Clone>(backend: &Backend<B>) -> (SharedTensor<T>, SharedTensor<T>, SharedTensor<T>, SharedTensor<T>){
+        let val = cast::<f64, T>(1f64).unwrap();
+        let val2 = cast::<f64, T>(2f64).unwrap();
+        let mut x = SharedTensor::<T>::new(backend.device(), &(1, 1, 3)).unwrap();
+        write_to_memory(x.get_mut(backend.device()).unwrap(), &[val, val, val2]);
+
+        let mut x_diff = SharedTensor::<T>::new(backend.device(), &(1, 1, 3)).unwrap();
+        write_to_memory(x_diff.get_mut(backend.device()).unwrap(), &[val, val, val2]);
+
+        let mut result = SharedTensor::<T>::new(backend.device(), &(1, 1, 3)).unwrap();
+        write_to_memory(result.get_mut(backend.device()).unwrap(), &[val, val, val2]);
+
+        let result_diff = SharedTensor::<T>::new(backend.device(), &(1, 1, 3)).unwrap();
+
+        (x, x_diff, result, result_diff)
+    }
+
+    #[test]
+    #[ignore]
+    fn it_computes_correct_lrn_on_cuda_for_f32() {
+        let backend = get_native_backend();
+        let (mut x, mut result) = get_memory::<f32, Native>(&backend);
+
+        let conf = NN::<f64>::new_lrn_config(&backend, 1u32, 1e-4f64, 0.75f64, 2f64).unwrap();
+        match backend.lrn(&mut x, &mut result, &conf) {
+            Ok(_) => {
+                if let Some(mem) = result.get(backend.device()).unwrap().as_native() {
+                    assert_eq!(&[0.59458125f32, 0.59458125f32, 1.1890286f32], mem.as_slice::<f32>());
+                }
+            },
+            Err(err) => { println!("{:?}", err); assert!(false) }
+        }
+    }
+
+    #[test]
+    #[ignore]
+    fn it_computes_correct_lrn_on_cuda_for_f64() {
+        let backend = get_native_backend();
+        let (mut x, mut result) = get_memory::<f64, Native>(&backend);
+
+        let conf = NN::<f64>::new_lrn_config(&backend, 1u32, 1e-4f64, 0.75f64, 2f64).unwrap();
+        match backend.lrn(&mut x, &mut result, &conf) {
+            Ok(_) => {
+                if let Some(mem) = result.get(backend.device()).unwrap().as_native() {
+                    assert_eq!(&[0.594581260843431f64, 0.594581260843431f64, 1.1890287651464355f64], mem.as_slice::<f64>());
+                }
+            },
+            Err(err) => { println!("{:?}", err); assert!(false) }
+        }
+    }
+
+    #[test]
+    #[ignore]
+    fn it_computes_correct_lrn_on_native_for_f32_plain() {
+        let backend = get_native_backend();
+        let (mut x, mut result) = get_memory::<f32, Native>(&backend);
+
+        let conf = NN::<f64>::new_lrn_config(&backend, 1u32, 1e-4f64, 0.75f64, 2f64).unwrap();
+        match backend.lrn_plain(&mut x, &mut result, &conf) {
+            Ok(_) => {
+                if let Some(mem) = result.get(backend.device()).unwrap().as_native() {
+                    assert_eq!(&[0.59458125f32, 0.59458125f32, 1.1890286f32], mem.as_slice::<f32>());
+                }
+            },
+            Err(err) => { println!("{:?}", err); assert!(false) }
+        }
+    }
+
+    #[test]
+    #[ignore]
+    fn it_computes_correct_lrn_on_native_for_f64_plain() {
+        let backend = get_native_backend();
+        let (mut x, mut result) = get_memory::<f64, Native>(&backend);
+
+        let conf = NN::<f64>::new_lrn_config(&backend, 1u32, 1e-4f64, 0.75f64, 2f64).unwrap();
+        match backend.lrn_plain(&mut x, &mut result, &conf) {
+            Ok(_) => {
+                if let Some(mem) = result.get(backend.device()).unwrap().as_native() {
+                    assert_eq!(&[0.594581260843431f64, 0.594581260843431f64, 1.1890287651464355f64], mem.as_slice::<f64>());
+                }
+            },
+            Err(err) => { println!("{:?}", err); assert!(false) }
+        }
+    }
+
+    #[test]
+    #[ignore]
+    fn it_computes_correct_lrn_grad_on_native_for_f32() {
+        let backend = get_native_backend();
+        let (mut x, mut x_diff, mut result, mut result_diff) = get_grad_memory::<f32, Native>(&backend);
+
+        let conf = NN::<f64>::new_lrn_config(&backend, 1u32, 1e-4f64, 0.75f64, 2f64).unwrap();
+        match backend.lrn_grad(&mut x, &mut x_diff, &mut result, &mut result_diff, &conf) {
+            Ok(_) => {
+                if let Some(mem) = result_diff.get(backend.device()).unwrap().as_native() {
+                    assert_eq!(&[0.59453666f32, 0.59453666f32, 1.188672f32], mem.as_slice::<f32>());
+                }
+            },
+            Err(err) => { println!("{:?}", err); assert!(false) }
+        }
+    }
+
+    #[test]
+    #[ignore]
+    fn it_computes_correct_lrn_grad_on_native_for_f64() {
+        let backend = get_native_backend();
+        let (mut x, mut x_diff, mut result, mut result_diff) = get_grad_memory::<f64, Native>(&backend);
+
+        let conf = NN::<f64>::new_lrn_config(&backend, 1u32, 1e-4f64, 0.75f64, 2f64).unwrap();
+        match backend.lrn_grad(&mut x, &mut x_diff, &mut result, &mut result_diff, &conf) {
+            Ok(_) => {
+                if let Some(mem) = result_diff.get(backend.device()).unwrap().as_native() {
+                    assert_eq!(&[0.594536669478436f64, 0.594536669478436f64, 1.188672127844352f64], mem.as_slice::<f64>());
+                }
+            },
+            Err(err) => { println!("{:?}", err); assert!(false) }
+        }
+    }
+
+    #[test]
+    #[ignore]
+    fn it_computes_correct_lrn_grad_on_native_for_f32_plain() {
+        let backend = get_native_backend();
+        let (mut x, mut x_diff, mut result, mut result_diff) = get_grad_memory::<f32, Native>(&backend);
+
+        let conf = NN::<f64>::new_lrn_config(&backend, 1u32, 1e-4f64, 0.75f64, 2f64).unwrap();
+        match backend.lrn_grad_plain(&mut x, &mut x_diff, &mut result, &mut result_diff, &conf) {
+            Ok(_) => {
+                if let Some(mem) = result_diff.get(backend.device()).unwrap().as_native() {
+                    assert_eq!(&[0.59453666f32, 0.59453666f32, 1.188672f32], mem.as_slice::<f32>());
+                }
+            },
+            Err(err) => { println!("{:?}", err); assert!(false) }
+        }
+    }
+
+    #[test]
+    #[ignore]
+    fn it_computes_correct_lrn_grad_on_native_for_f64_plain() {
+        let backend = get_native_backend();
+        let (mut x, mut x_diff, mut result, mut result_diff) = get_grad_memory::<f64, Native>(&backend);
+
+        let conf = NN::<f64>::new_lrn_config(&backend, 1u32, 1e-4f64, 0.75f64, 2f64).unwrap();
+        match backend.lrn_grad_plain(&mut x, &mut x_diff, &mut result, &mut result_diff, &conf) {
+            Ok(_) => {
+                if let Some(mem) = result_diff.get(backend.device()).unwrap().as_native() {
                     assert_eq!(&[0.594536669478436f64, 0.594536669478436f64, 1.188672127844352f64], mem.as_slice::<f64>());
                 }
             },
