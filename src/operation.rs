44cbacf21c0857dc32acba714c973bf7ffae8dff
Hobofan <goisser94@gmail.com>
2016-02-22 10:36:07 +0100

    feat/iblas_trait: remove trait bounds for IBlas
    CLOSES #7

--- src/operation.rs
+++ src/operation.rs
@@ -1,54 +1,53 @@
 //! Provides the IOperationX operation traits for Collenchyma's Framework implementation.
 
-use collenchyma::plugin::numeric_helpers::Float;
 use collenchyma::memory::MemoryType;
 use collenchyma::plugin::Error;
 use ::transpose::Transpose;
 
 /// Describes a Asum Operation.
-pub trait IOperationAsum<F: Float> {
+pub trait IOperationAsum<F> {
     /// Computes the Asum operation.
     fn compute(&self, x: &MemoryType, result: &mut MemoryType) -> Result<(), Error>;
 }
 
 /// Describes a Axpy Operation.
-pub trait IOperationAxpy<F: Float> {
+pub trait IOperationAxpy<F> {
     /// Computes the Axpy operation.
     fn compute(&self, a: &MemoryType, x: & MemoryType, y: &mut MemoryType) -> Result<(), Error>;
 }
 
 /// Describes a Copy Operation.
-pub trait IOperationCopy<F: Float> {
+pub trait IOperationCopy<F> {
     /// Computes the Copy operation.
     fn compute(&self, x: &MemoryType, y: &mut MemoryType) -> Result<(), Error>;
 }
 
 /// Describes a Dot Operation.
-pub trait IOperationDot<F: Float> {
+pub trait IOperationDot<F> {
     /// Computes the Dot operation.
     fn compute(&self, x: &MemoryType, y: &MemoryType, result: &mut MemoryType) -> Result<(), Error>;
 }
 
 /// Describes a Nrm2 Operation.
-pub trait IOperationNrm2<F: Float> {
+pub trait IOperationNrm2<F> {
     /// Computes the Nrm2 operation.
     fn compute(&self, x: &MemoryType, result: &mut MemoryType) -> Result<(), Error>;
 }
 
 /// Describes a Scale Operation.
-pub trait IOperationScale<F: Float> {
+pub trait IOperationScale<F> {
     /// Computes the Scale operation.
     fn compute(&self, a: &MemoryType, x: &mut MemoryType) -> Result<(), Error>;
 }
 
 /// Describes a Swap Operation.
-pub trait IOperationSwap<F: Float> {
+pub trait IOperationSwap<F> {
     /// Computes the Swap operation.
     fn compute(&self, x: &mut MemoryType, y: &mut MemoryType) -> Result<(), Error>;
 }
 
 /// Describes a Gemm Operation.
-pub trait IOperationGemm<F: Float> {
+pub trait IOperationGemm<F> {
     /// Computes the Gemm operation.
     fn compute(&self, alpha: &MemoryType, at: Transpose, a_dims: &[usize], a: &MemoryType, bt: Transpose, b_dims: &[usize], b: &MemoryType, beta: &MemoryType, c_dims: &[usize], c: &mut MemoryType) -> Result<(), ::collenchyma::error::Error>;
 }
