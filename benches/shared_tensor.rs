4322f2087232e1e97f37d9641330cc345e04737f
Alexander Morozov <etesial@gmail.com>
2016-04-21 00:08:33 +0300

    refactor/benches: refactor SharedTensor benchmarks
    Refactoring cleans up code duplication, adds values in MB/s to output, adds
    separate benches for unidirectional transfers (GPU -> Host, Host -> GPU),
    adds benchmarks for 1KB transfers.

--- benches/shared_tensor.rs
+++ benches/shared_tensor.rs
@@ -1,42 +1,26 @@
 #![feature(test)]
-#![feature(clone_from_slice)]
 
 extern crate test;
 extern crate collenchyma as co;
 extern crate rand;
 
 use test::Bencher;
-use co::device::{IDevice, DeviceType};
+use co::device::IDevice;
 use co::backend::{Backend, BackendConfig};
-#[cfg(feature = "native")]
-use co::frameworks::Native;
-#[cfg(feature = "native")]
-use co::frameworks::native;
-#[cfg(feature = "opencl")]
-use co::frameworks::OpenCL;
-#[cfg(feature = "opencl")]
-use co::frameworks::opencl;
-#[cfg(feature = "cuda")]
-use co::frameworks::Cuda;
-#[cfg(feature = "cuda")]
-use co::frameworks::cuda;
 use co::framework::IFramework;
 use co::tensor::SharedTensor;
 
-use rand::{thread_rng, Rng};
+#[cfg(feature = "native")]
+use co::frameworks::Native;
+#[cfg(feature = "opencl")]
+use co::frameworks::OpenCL;
+#[cfg(feature = "cuda")]
+use co::frameworks::Cuda;
 
 #[cfg(feature = "native")]
 fn native_backend() -> Backend<Native> {
     let framework = Native::new();
     let hardwares = framework.hardwares().to_vec();
-    let backend_config = BackendConfig::new(framework, &hardwares);
-    Backend::new(backend_config).unwrap()
-}
-
-#[cfg(feature = "cuda")]
-fn cuda_backend() -> Backend<Cuda> {
-    let framework = Cuda::new();
-    let hardwares = framework.hardwares()[0..1].to_vec();
     let backend_config = BackendConfig::new(framework, &hardwares);
     Backend::new(backend_config).unwrap()
 }
@@ -49,120 +33,187 @@ fn opencl_backend() -> Backend<OpenCL> {
     Backend::new(backend_config).unwrap()
 }
 
-#[inline(never)]
-fn sync_back_and_forth(
-    b: &mut Bencher,
-    n: usize,
-    nt_device: &DeviceType,
-    cl_device: &DeviceType,
-    mem: &mut SharedTensor<u8>
-) {
+#[cfg(feature = "cuda")]
+fn cuda_backend() -> Backend<Cuda> {
+    let framework = Cuda::new();
+    let hardwares = framework.hardwares()[0..1].to_vec();
+    let backend_config = BackendConfig::new(framework, &hardwares);
+    Backend::new(backend_config).unwrap()
+}
+
+fn sync_back_and_forth<F1, F2>(b: &mut Bencher,
+                               backend1: Backend<F1>, backend2: Backend<F2>,
+                               mem_size: usize)
+    where F1: co::IFramework + Clone,
+          F2: co::IFramework + Clone {
+
+    let dev1 = backend1.device();
+    let dev2 = backend2.device();
+
+    let mem = &mut SharedTensor::<u8>::new(&mem_size);
+    /* initialize and warm-up */
+    mem.write_only(&dev2).unwrap();
+    mem.read_write(&dev1).unwrap();
+    mem.read_write(&dev2).unwrap();
+
+    b.bytes = mem_size as u64 * 2; // we do two transfers per iteration
     b.iter(|| {
-        for _ in 0..n {
-            match mem.read_write(&cl_device) {
-                Ok(_) => assert!(true),
-                Err(err) => {
-                    println!("{:?}", err);
-                    assert!(false);
-                }
-            }
-            match mem.read_write(&nt_device) {
-                Ok(_) => assert!(true),
-                Err(err) => {
-                    println!("{:?}", err);
-                    assert!(false);
-                }
-            }
-        }
+        mem.read_write(&dev1).unwrap();
+        mem.read_write(&dev2).unwrap();
     });
 }
 
-#[bench]
+fn unidirectional_sync<F1, F2>(b: &mut Bencher,
+                               src_backend: Backend<F1>, dst_backend: Backend<F2>,
+                               mem_size: usize)
+    where F1: co::IFramework + Clone,
+          F2: co::IFramework + Clone {
+
+    let src_dev = src_backend.device();
+    let dst_dev = dst_backend.device();
+
+    let mem = &mut SharedTensor::<u8>::new(&mem_size);
+    /* initialize and warm-up */
+    mem.write_only(&src_dev).unwrap();
+    mem.read(&dst_dev).unwrap();
+
+    b.bytes = mem_size as u64;
+    b.iter(|| {
+        mem.write_only(&src_dev).unwrap();
+        mem.read(&dst_dev).unwrap();
+    });
+}
+
+#[cfg(feature = "native")]
 #[cfg(feature = "opencl")]
-fn bench_256_alloc_1mb_opencl(b: &mut Bencher) {
-    let opencl_backend = opencl_backend();
-    if let &DeviceType::OpenCL(ref cl_device) = opencl_backend.device() {
-        bench_256_alloc_1mb_opencl_profile(b, cl_device, 1_048_576);
-    } else {
-        assert!(false);
+mod opencl_and_native {
+    use test::Bencher;
+    use co::device::{IDevice, DeviceType};
+    use co::frameworks::opencl;
+    use super::{native_backend, opencl_backend,
+                sync_back_and_forth, unidirectional_sync};
+
+    #[inline(never)]
+    fn bench_256_alloc_1mb_opencl_profile(
+        b: &mut Bencher,
+        device: &opencl::Context,
+        size: usize
+    ) {
+        b.iter(|| {
+            for _ in 0..256 {
+                device.alloc_memory(size).unwrap();
+            }
+        });
+    }
+
+    #[bench]
+    fn bench_256_alloc_1mb_opencl(b: &mut Bencher) {
+        let opencl_backend = opencl_backend();
+        if let &DeviceType::OpenCL(ref cl_device) = opencl_backend.device() {
+            bench_256_alloc_1mb_opencl_profile(b, cl_device, 1_048_576);
+        } else {
+            assert!(false);
+        }
+    }
+
+    #[bench]
+    fn bench_sync_1kb_native_opencl_back_and_forth(b: &mut Bencher) {
+        sync_back_and_forth(b, opencl_backend(), native_backend(), 1024);
+    }
+
+    #[bench]
+    fn bench_sync_1kb_native_to_opencl(b: &mut Bencher) {
+        unidirectional_sync(b, native_backend(), opencl_backend(), 1024);
+    }
+
+    #[bench]
+    fn bench_sync_1kb_opencl_to_native(b: &mut Bencher) {
+        unidirectional_sync(b, opencl_backend(), native_backend(), 1024);
+    }
+
+    #[bench]
+    fn bench_sync_1mb_native_opencl_back_and_forth(b: &mut Bencher) {
+        sync_back_and_forth(b, opencl_backend(), native_backend(), 1_048_576);
+    }
+
+    #[bench]
+    fn bench_sync_1mb_native_to_opencl(b: &mut Bencher) {
+        unidirectional_sync(b, native_backend(), opencl_backend(), 1_048_576);
+    }
+
+    #[bench]
+    fn bench_sync_1mb_opencl_to_native(b: &mut Bencher) {
+        unidirectional_sync(b, opencl_backend(), native_backend(), 1_048_576);
+    }
+
+    #[bench]
+    fn bench_sync_128mb_native_opencl_back_and_forth(b: &mut Bencher) {
+        sync_back_and_forth(b, opencl_backend(), native_backend(), 128 * 1_048_576);
+    }
+
+    #[bench]
+    fn bench_sync_128mb_native_to_opencl(b: &mut Bencher) {
+        unidirectional_sync(b, native_backend(), opencl_backend(), 128 * 1_048_576);
+    }
+
+    #[bench]
+    fn bench_sync_128mb_opencl_to_native(b: &mut Bencher) {
+        unidirectional_sync(b, opencl_backend(), native_backend(), 128 * 1_048_576);
     }
 }
 
-#[inline(never)]
-#[cfg(feature = "opencl")]
-fn bench_256_alloc_1mb_opencl_profile(
-    b: &mut Bencher,
-    device: &opencl::Context,
-    size: usize
-) {
-    b.iter(|| {
-        for _ in 0..256 {
-            device.alloc_memory(size);
-        }
-    });
-}
 
-#[bench]
-#[cfg(feature = "opencl")]
-fn bench_256_sync_1mb_native_opencl(b: &mut Bencher) {
-    let opencl_backend = opencl_backend();
-    let cl_device = opencl_backend.device();
-    let native_backend = native_backend();
-    let nt_device = native_backend.device();
-    // if let &DeviceType::OpenCL(ref cl_d) = cl_device {
-    //     println!("{:?}", cl_d.hardwares()[0].clone().load_name());
-    // }
-    let mem = &mut SharedTensor::<u8>::new(&1_048_576);
-    mem.write_only(&cl_device);
-    bench_256_sync_1mb_native_opencl_profile(b, nt_device, cl_device, mem);
-}
-
-#[inline(never)]
-#[cfg(feature = "opencl")]
-fn bench_256_sync_1mb_native_opencl_profile(b: &mut Bencher, nt_device: &DeviceType, cl_device: &DeviceType, mem: &mut SharedTensor<u8>) {
-    sync_back_and_forth(b, 256, nt_device, cl_device, mem)
-}
-
-#[bench]
+#[cfg(feature = "native")]
 #[cfg(feature = "cuda")]
-fn bench_256_sync_1mb_native_cuda(b: &mut Bencher) {
-    let cuda_backend = cuda_backend();
-    let cl_device = cuda_backend.device();
-    let native_backend = native_backend();
-    let nt_device = native_backend.device();
-    // if let &DeviceType::Cuda(ref cl_d) = cl_device {
-    //     println!("{:?}", cl_d.hardwares()[0].clone().load_name());
-    // }
-    let mem = &mut SharedTensor::<u8>::new(&1_048_576);
-    mem.write_only(&cl_device);
-    bench_256_sync_1mb_native_cuda_profile(b, nt_device, cl_device, mem);
-}
+mod cuda_and_native {
+    use test::Bencher;
+    use super::{native_backend, cuda_backend,
+                sync_back_and_forth, unidirectional_sync};
 
-#[inline(never)]
-#[cfg(feature = "cuda")]
-fn bench_256_sync_1mb_native_cuda_profile(b: &mut Bencher, nt_device: &DeviceType, cl_device: &DeviceType, mem: &mut SharedTensor<u8>) {
-    sync_back_and_forth(b, 256, nt_device, cl_device, mem)
-}
+    #[bench]
+    fn bench_sync_1kb_native_cuda_back_and_forth(b: &mut Bencher) {
+        sync_back_and_forth(b, cuda_backend(), native_backend(), 1024);
+    }
 
-#[bench]
-#[cfg(feature = "cuda")]
-fn bench_2_sync_128mb_native_cuda(b: &mut Bencher) {
-    let cuda_backend = cuda_backend();
-    let cl_device = cuda_backend.device();
-    let native_backend = native_backend();
-    let nt_device = native_backend.device();
-    // if let &DeviceType::Cuda(ref cl_d) = cl_device {
-    //     println!("{:?}", cl_d.hardwares()[0].clone().load_name());
-    // }
-    let mem = &mut SharedTensor::<u8>::new(&(128 * 1_048_576));
-    mem.write_only(&cl_device);
-    bench_2_sync_128mb_native_cuda_profile(b, nt_device, cl_device, mem);
-}
+    #[bench]
+    fn bench_sync_1kb_native_to_cuda(b: &mut Bencher) {
+        unidirectional_sync(b, native_backend(), cuda_backend(), 1024);
+    }
 
-#[inline(never)]
-#[cfg(feature = "cuda")]
-fn bench_2_sync_128mb_native_cuda_profile(b: &mut Bencher, nt_device: &DeviceType, cl_device: &DeviceType, mem: &mut SharedTensor<u8>) {
-    sync_back_and_forth(b, 2, nt_device, cl_device, mem)
+    #[bench]
+    fn bench_sync_1kb_cuda_to_native(b: &mut Bencher) {
+        unidirectional_sync(b, cuda_backend(), native_backend(), 1024);
+    }
+
+    #[bench]
+    fn bench_sync_1mb_native_cuda_back_and_forth(b: &mut Bencher) {
+        sync_back_and_forth(b, cuda_backend(), native_backend(), 1_048_576);
+    }
+
+    #[bench]
+    fn bench_sync_1mb_native_to_cuda(b: &mut Bencher) {
+        unidirectional_sync(b, native_backend(), cuda_backend(), 1_048_576);
+    }
+
+    #[bench]
+    fn bench_sync_1mb_cuda_to_native(b: &mut Bencher) {
+        unidirectional_sync(b, cuda_backend(), native_backend(), 1_048_576);
+    }
+
+    #[bench]
+    fn bench_sync_128mb_native_cuda_back_and_forth(b: &mut Bencher) {
+        sync_back_and_forth(b, cuda_backend(), native_backend(), 128 * 1_048_576);
+    }
+
+    #[bench]
+    fn bench_sync_128mb_native_to_cuda(b: &mut Bencher) {
+        unidirectional_sync(b, native_backend(), cuda_backend(), 128 * 1_048_576);
+    }
+
+    #[bench]
+    fn bench_sync_128mb_cuda_to_native(b: &mut Bencher) {
+        unidirectional_sync(b, cuda_backend(), native_backend(), 128 * 1_048_576);
+    }
 }
 
 #[bench]
