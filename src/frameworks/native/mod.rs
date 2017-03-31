39d4f08b8a56c6aa33c32d30fd48fed5d3636db2
Bernhard Schuster <bernhard@ratpoison.io>
2017-03-31 09:47:04 +0200

    refactor/conv: use for loops in native convolution

--- src/frameworks/native/mod.rs
+++ src/frameworks/native/mod.rs
@@ -126,32 +126,14 @@ impl<'a, T> NNOperationConfig<T> for helper::ConvolutionConfig
                  return Err(Error::Plugin(PluginError::Plugin("Unimplemented.")));
              },
          }
- 
+
          Ok(helper::ConvolutionConfig {
              filter_shape: filter.desc().clone(),
              stride: stride.to_vec(),
              padding: zero_padding.to_vec(),
          })
      }
-     //fn convolution(&self, filter: &mut SharedTensor<T>,
-                    //input: &mut SharedTensor<T>,
-                    //output: &mut SharedTensor<T>,
-                    //scratch: &mut SharedTensor<u8>,
-                    //config: &Self::CC) -> Result<(), Error>
-     //{
-         //let dev = self.device();
-         //let _ = input.add_device(dev);
-         //try!(input.sync(dev));
-         //let _ = filter.add_device(dev);
-         //try!(filter.sync(dev));
-         //let _ = output.add_device(dev);
-         //try!(output.sync(dev));
-         //let _ = scratch.add_device(dev);
-         //try!(scratch.sync(dev));
- 
-         //self.convolution_plain(filter, input, output, scratch, config)
-     //}
- 
+
     fn convolution(&self, filter: &SharedTensor<T>,
                          x: &SharedTensor<T>,
                          result: &mut SharedTensor<T>,
@@ -159,33 +141,32 @@ impl<'a, T> NNOperationConfig<T> for helper::ConvolutionConfig
                          config: &Self::CC) -> Result<(), Error>
     {
         let dev = self.device();
- 
+
         let input_dim = x.desc();
         let input = x.read(dev).unwrap()
             .as_native().unwrap()
             .as_slice::<T>();
         let input_stride = input_dim.default_stride();
- 
+
         let output_dim = result.desc().clone();
         // this is ok, we only read parts we already wrote
         let output = result.write_only(dev).unwrap()
             .as_mut_native().unwrap()
             .as_mut_slice::<T>();
         let output_stride = output_dim.default_stride();
- 
+
         {
             for o in output.iter_mut() {
                 *o = Default::default();
             }
         }
- 
+
         let filter_dim = filter.desc();
         let filter = filter.read(dev).unwrap()
             .as_native().unwrap()
             .as_slice::<T>();
         let filter_stride = filter_dim.default_stride();
- 
- 
+
         // sanity check
         assert!(input_dim[0] == output_dim[0]);
         assert!(filter_dim[0] == output_dim[1]);
@@ -196,26 +177,23 @@ impl<'a, T> NNOperationConfig<T> for helper::ConvolutionConfig
         // recursively sum up elementwise multiplication of the hyperplanes.
         fn filter_<T>(input: &[T], input_stride: &[usize], input_dim: &[usize],
                       input_offset: usize, input_idx_base: &[usize],
- 
                       filter: &[T], filter_stride: &[usize], filter_dim: &[usize],
                       filter_offset: usize,
- 
                       padding: &[i32],
                       depth: usize, depth_end: usize,
                       acc: Option<T>) -> T
             where T: Add<T, Output = T> + Mul<T, Output = T> + Default + Copy,
         {
             let mut acc = acc.unwrap_or_default();
- 
+
             let p = padding[0] as usize;
             let input_idx_end = input_dim[0] + 2 * p;
- 
-            let mut input_idx = input_idx_base[0];
-            let mut filter_idx = 0;
-            while filter_idx < filter_dim[0] {
+
+            for filter_idx in 0..filter_dim[0] {
+                let input_idx = input_idx_base[0] + filter_idx;
                 let i_offset = input_offset + (input_idx - p) * input_stride[0];
                 let f_offset = filter_offset + filter_idx * filter_stride[0];
- 
+
                 let v = if input_idx < p || input_idx + 1 > input_idx_end - p {
                     Default::default()
                 } else if depth + 1 >= depth_end {
@@ -225,31 +203,23 @@ impl<'a, T> NNOperationConfig<T> for helper::ConvolutionConfig
                             i_offset, &input_idx_base[1..],
                             filter, &filter_stride[1..], &filter_dim[1..],
                             f_offset,
-                            &padding[1..], depth + 1, depth_end,
+                            &padding[1..],
+                            depth + 1, depth_end,
                             None)
                 };
- 
                 acc = acc + v;
- 
-                input_idx += 1;
-                filter_idx += 1;
             }
- 
             return acc;
         }
- 
- 
+
         // depth == 0 is the first level
         fn conv<T>(input: &[T], input_stride: &[usize], input_dim: &[usize],
                    top_input_offset: usize, input_offset: usize,
                    input_idx_base: &mut [usize],
- 
                    filter: &[T], filter_stride: &[usize], filter_dim: &[usize],
                    filter_offset: usize,
- 
                    depth: usize,
                    padding: &[i32], stride: &[i32],
- 
                    output: &mut [T], output_stride: &[usize],
                    output_dim: &[usize],
                    output_offset: usize)
@@ -257,15 +227,13 @@ impl<'a, T> NNOperationConfig<T> for helper::ConvolutionConfig
         {
             let p = padding[depth] as usize;
             //let input_end = input_dim[depth] + 2 * p - (filter_dim[depth]);
- 
-            let mut input_i = 0;
- 
-            let mut output_idx = 0;
-            while output_idx < output_dim[0] {
+
+            for output_idx in 0..output_dim[0] {
+                let input_i = output_idx * stride[0] as usize;
                 input_idx_base[depth] = input_i;
                 let input_offset = input_offset + input_i * input_stride[depth];
                 let output_offset = output_offset + output_idx * output_stride[0];
- 
+
                 if depth + 1 < input_dim.len() {
                     conv(input, input_stride, input_dim, top_input_offset,
                          input_offset, input_idx_base,
@@ -281,20 +249,14 @@ impl<'a, T> NNOperationConfig<T> for helper::ConvolutionConfig
                                     None);
                     output[output_offset] = output[output_offset] + v;
                 }
- 
-                input_i += stride[0] as usize;
-                output_idx += 1;
             }
         }
- 
+
         fn conv_k_d1<T>(_batch: usize,
                         input: &[T], input_stride: &[usize], input_dim: &[usize],
                         input_offset: usize, input_idx_base: &mut [usize],
- 
                         filter: &[T], filter_stride: &[usize], filter_dim: &[usize],
- 
                         padding: &[i32], stride: &[i32],
- 
                         output: &mut [T], output_stride: &[usize],
                         output_dim: &[usize], output_offset: usize)
             where T: Add<T, Output = T> + Mul<T, Output = T> + Default + Copy,
@@ -305,7 +267,7 @@ impl<'a, T> NNOperationConfig<T> for helper::ConvolutionConfig
                 for d1 in 0..input_dim[0] {
                     let input_offset = input_offset + d1 * input_stride[0];
                     let filter_offset = filter_offset + d1 * filter_stride[1];
- 
+
                     conv(input, &input_stride[1..], &input_dim[1..],
                          input_offset, input_offset, input_idx_base,
                          filter, &filter_stride[2..], &filter_dim[2..], filter_offset,
@@ -315,31 +277,28 @@ impl<'a, T> NNOperationConfig<T> for helper::ConvolutionConfig
                 }
             }
         }
- 
+
         let mut input_idx = Vec::new();
         input_idx.resize(input_dim.len() - 2, 0);
         let mut output_idx = Vec::new();
         output_idx.resize(output_dim.len(), 0);
- 
+
         let batches = input_dim[0];
-        let mut batch = 0;
-        while batch < batches {
+        for batch in 0..batches {
             let input_offset = batch * input_stride[0];
             let output_offset = batch * output_stride[0];
- 
+
             conv_k_d1(batch, input, &input_stride[1..], &input_dim[1..], input_offset,
                       &mut input_idx[..],
                       filter, &filter_stride[..], &filter_dim[..],
                       &config.padding[..], &config.stride[..],
                       output, &output_stride[1..], &output_dim[1..],
                       output_offset);
- 
-            batch += 1;
         }
- 
+
         Ok(())
     }
- 
+
     fn convolution_grad_filter(&self, src_data: &SharedTensor<T>,
                                dest_diff: &SharedTensor<T>,
                                filter_diff: &mut SharedTensor<T>,
@@ -387,15 +346,15 @@ where T: Add<T, Output = T> + Mul<T, Output = T> + Default + Copy + PartialOrd,
               config: &Self::CPOOL
               ) -> Result<(), ::co::error::Error>{
                 let dev = self.device();
- 
+
                 let input_dim = x.desc(); // [4, 4, 4, 4]
                 let input = x.read(dev).unwrap()
                     .as_native().unwrap()
                     .as_slice::<T>();
                 let input_stride = input_dim.default_stride(); // [64, 16, 4, 1];
- 
+
                 let output_dim = result.desc().clone(); // [4,4,2,2]
-        // this is ok, we only read parts we already wrote
+                // this is ok, we only read parts we already wrote
                 let output = result.write_only(dev).unwrap()
                     .as_mut_native().unwrap()
                     .as_mut_slice::<T>();
