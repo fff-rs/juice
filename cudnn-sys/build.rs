8216ad12c465298489eb5df739a5f2735174c53c
Bernhard Schuster <bernhard@ahoi.io>
2017-06-19 11:01:51 +0200

    fix/bindgen: hide max_align_t
    See https://github.com/servo/rust-bindgen/issues/550 for details.

--- cudnn-sys/build.rs
+++ cudnn-sys/build.rs
@@ -62,6 +62,7 @@ fn main() {
             // requires a nightly rustc and enabling
             // unstable features.
             .no_unstable_rust()
+            .hide_type("max_align_t") // https://github.com/servo/rust-bindgen/issues/550
             .raw_line(r"
 //! Defines the FFI for CUDA cuDNN.
 //!
