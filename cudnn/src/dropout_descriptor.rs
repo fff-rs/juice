ac152c3fe0146ae2ca0c5033a0bd0dc76395af90
Bernhard Schuster <bernhard@ahoi.io>
2017-08-05 19:37:44 +0200

    feat/dropout: wrap cuda device memory and dropout

---
+++ cudnn/src/dropout_descriptor.rs
@@ -0,0 +1,51 @@
+//! Defines a Dropout Descriptor.
+//!
+//! A Tensor Descriptor is used to hold information about the probability
+//! of dropping a value as well as an initial seed.
+
+use super::{API, Error};
+use ffi::*;
+use cudnn::Cudnn;
+
+#[derive(Debug, Clone)]
+/// Describes a DropoutDescriptor.
+pub struct DropoutDescriptor {
+    id: cudnnDropoutDescriptor_t,
+}
+
+impl Drop for DropoutDescriptor {
+    #[allow(unused_must_use)]
+    fn drop(&mut self) {
+        API::destroy_dropout_descriptor(*self.id_c());
+    }
+}
+
+impl DropoutDescriptor {
+    /// Initializes a new CUDA cuDNN Dropout Descriptor.
+    pub fn new(handle : &Cudnn, dropout: f32, seed: u64) -> Result<DropoutDescriptor, Error> {
+        let generic_dropout_desc = API::create_dropout_descriptor()?;
+	let states: *mut ::libc::c_void = ::std::ptr::null_mut();
+	let state_size_in_bytes : usize = 0;
+            // ::cudaHostAlloc(&mut x, 2 * 2 * 2, 0);
+        API::set_dropout_descriptor(
+            generic_dropout_desc,
+            *handle.id_c(),
+            dropout,
+            states,
+            state_size_in_bytes,
+            seed,
+        )?;
+
+        Ok(DropoutDescriptor::from_c(generic_dropout_desc))
+    }
+
+    /// Initializes a new CUDA cuDNN Tensor Descriptor from its C type.
+    pub fn from_c(id: cudnnDropoutDescriptor_t) -> DropoutDescriptor {
+        DropoutDescriptor { id: id }
+    }
+
+    /// Returns the CUDA cuDNN Tensor Descriptor as its C type.
+    pub fn id_c(&self) -> &cudnnDropoutDescriptor_t {
+        &self.id
+    }
+}
