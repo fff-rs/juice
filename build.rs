2673c8cfdc1f15b01d97388177584cd92458a746
Bernhard Schuster <bernhard@ahoi.io>
2017-08-30 19:57:55 +0200

    fix/build: use env vars for variant and pkg-config for lib paths

--- build.rs
+++ build.rs
@@ -1,15 +1,35 @@
-use std::env::var;
+extern crate pkg_config;
+use std::env;
 
 fn main() {
-    let link_type = env_or_default("CARGO_BLAS_TYPE", "dylib");
-    let link_name = env_or_default("CARGO_BLAS", "openblas");
+    let variant = env::var("BLAS_VARIANT").unwrap_or("openblas".to_string());
+    let lib_dir = env::var("BLAS_LIB_DIR").ok();
+    let include_dir = env::var("BLAS_INCLUDE_DIR").ok();
 
-    println!("cargo:rustc-link-lib={}={}", link_type, link_name);
-}
+    if lib_dir.is_none() && include_dir.is_none() {
+        if let Ok(info) = pkg_config::find_library(variant.as_str()) {
+            // avoid empty include paths as they are not supported by GCC
+            if info.include_paths.len() > 0 {
+                let paths = env::join_paths(info.include_paths).unwrap();
+                println!("cargo:include={}", paths.to_str().unwrap());
+            }
+            return;
+        }
+    }
 
-fn env_or_default(var_name: &str, default: &str) -> String {
-    match var(var_name) {
-        Ok(s) => s,
-        Err(_) => default.into(),
+    let mode = if env::var_os("BLAS_STATIC").is_some() {
+        "static"
+    } else {
+        "dylib"
+    };
+
+    if let Some(lib_dir) = lib_dir {
+        println!("cargo:rustc-link-search=native={}", lib_dir);
+    }
+
+    println!("cargo:rustc-link-lib={}={}", mode, variant);
+
+    if let Some(include_dir) = include_dir.clone() {
+        println!("cargo:include={}", include_dir);
     }
 }
