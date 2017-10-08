b264a38c2bf318ecf2d6f0f9e3e2c79096377395
Bernhard Schuster <bernhard@ahoi.io>
2017-10-08 22:46:01 +0200

    feat/version: bump cudnn version to 7.0.3

--- cudnn-sys/src/lib.rs
+++ cudnn-sys/src/lib.rs
@@ -6,13 +6,14 @@ pub use generated::*;
 
 impl Default for cudnnConvolutionFwdAlgoPerf_t {
     fn default() -> Self {
-        cudnnConvolutionFwdAlgoPerf_t {
-            algo : cudnnConvolutionFwdAlgo_t::CUDNN_CONVOLUTION_FWD_ALGO_DIRECT,
-            status : cudnnStatus_t::CUDNN_STATUS_NOT_INITIALIZED,
-            time : 0.0 as f32,
-            memory : 0,
-            determinism : cudnnDeterminism_t::CUDNN_NON_DETERMINISTIC,
-            reserved : [0; 4usize],
+        Self {
+            algo: cudnnConvolutionFwdAlgo_t::CUDNN_CONVOLUTION_FWD_ALGO_DIRECT,
+            status: cudnnStatus_t::CUDNN_STATUS_NOT_INITIALIZED,
+            time: 0.0 as f32,
+            memory: 0,
+            determinism: cudnnDeterminism_t::CUDNN_NON_DETERMINISTIC,
+            mathType: cudnnMathType_t::CUDNN_DEFAULT_MATH,
+            reserved: [0; 3usize],
         }
     }
 }
@@ -20,29 +21,29 @@ impl Default for cudnnConvolutionFwdAlgoPerf_t {
 
 impl Default for cudnnConvolutionBwdFilterAlgoPerf_t {
     fn default() -> Self {
-        cudnnConvolutionBwdFilterAlgoPerf_t {
-            algo : cudnnConvolutionBwdFilterAlgo_t::CUDNN_CONVOLUTION_BWD_FILTER_ALGO_0,
-            status : cudnnStatus_t::CUDNN_STATUS_NOT_INITIALIZED,
-            time : 0.0 as f32,
-            memory : 0,
-            determinism : cudnnDeterminism_t::CUDNN_NON_DETERMINISTIC,
-            reserved: [0; 4usize],
+        Self {
+            algo: cudnnConvolutionBwdFilterAlgo_t::CUDNN_CONVOLUTION_BWD_FILTER_ALGO_0,
+            status: cudnnStatus_t::CUDNN_STATUS_NOT_INITIALIZED,
+            time: 0.0 as f32,
+            memory: 0,
+            determinism: cudnnDeterminism_t::CUDNN_NON_DETERMINISTIC,
+            mathType: cudnnMathType_t::CUDNN_DEFAULT_MATH,
+            reserved: [0; 3usize],
         }
     }
 }
 
 
 impl Default for cudnnConvolutionBwdDataAlgoPerf_t {
-   fn default() -> Self {
-        cudnnConvolutionBwdDataAlgoPerf_t {
-            algo : cudnnConvolutionBwdDataAlgo_t::CUDNN_CONVOLUTION_BWD_DATA_ALGO_0,
-            status : cudnnStatus_t::CUDNN_STATUS_NOT_INITIALIZED,
-            time : 0.0 as f32,
-            memory : 0,
-            determinism : cudnnDeterminism_t::CUDNN_NON_DETERMINISTIC,
-            reserved : [0; 4usize],
+    fn default() -> Self {
+        Self {
+            algo: cudnnConvolutionBwdDataAlgo_t::CUDNN_CONVOLUTION_BWD_DATA_ALGO_0,
+            status: cudnnStatus_t::CUDNN_STATUS_NOT_INITIALIZED,
+            time: 0.0 as f32,
+            memory: 0,
+            determinism: cudnnDeterminism_t::CUDNN_NON_DETERMINISTIC,
+            mathType: cudnnMathType_t::CUDNN_DEFAULT_MATH,
+            reserved: [0; 3usize],
         }
     }
 }
-
-
