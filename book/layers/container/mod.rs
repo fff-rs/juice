dfeab9209bc8a6d14f8c347caf6bf628c27e76d6
Sir Mergealot <sirmergealot@spearow.io>
2017-11-28 15:02:12 +0000

    doc/automatic: update

--- book/layers/container/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Provides container layers.
-//!
-//! For now layers in container should be discribed as layers that are used
-//! to connect multiple layers together to create 'networks'.
-
-pub use self::sequential::{Sequential, SequentialConfig};
-
-pub mod sequential;
