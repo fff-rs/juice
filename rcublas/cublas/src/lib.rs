1ae5f4f37c39d3216813c59c3701e584164b0f5d
Bernhard Schuster <bernhard@ahoi.io>
2020-04-05 00:48:47 +0200

    fix/cublas: use a set of handles to clean up at exit
    untested! Context creation still needs to be Arc'd

--- rcublas/cublas/src/lib.rs
+++ rcublas/cublas/src/lib.rs
@@ -5,6 +5,14 @@ extern crate rcublas_sys as ffi;
 #[cfg(test)]
 extern crate coaster as co;
 
+#[macro_use]
+extern crate lazy_static;
+
+extern crate spin;
+
+#[macro_use]
+extern crate ctor;
+
 pub use api::Context;
 pub use error::Error;
 
