e0e4240ac96aedb76fc47e73599a195d83b2c7f4
Bernhard Schuster <bernhard@ahoi.io>
2020-06-11 11:21:32 +0200

    chore/build: sane formatting for build.rs

--- juice/build.rs
+++ juice/build.rs
@@ -1,5 +1,7 @@
-extern crate capnpc;
-
 fn main() {
-    ::capnpc::CompilerCommand::new().src_prefix("capnp").file("capnp/juice.capnp").run().expect("schema compiler command");
+    capnpc::CompilerCommand::new()
+        .src_prefix("capnp")
+        .file("capnp/juice.capnp")
+        .run()
+        .expect("capnpc schema compiler command must succeed");
 }
