edddfc9a2d2403c58174fae08b73ae41575eef51
Sir Mergealot <sirmergealot@spearow.io>
2021-03-27 15:30:07 +0000

    docs/automatic: update

--- implementors/greenglas/modifier/trait.Set.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["greenglas"] = [{"text":"impl Set for Image","synthetic":false,"types":[]},{"text":"impl Set for Word","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- juice/solver/confusion_matrix/sidebar-items.js
+++ juice/solver/confusion_matrix/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Accuracy","The accuracy of the predictions in a ConfusionMatrix."],["ConfusionMatrix","A [ConfusionMatrix][wiki]."],["Sample","A single prediction Sample."]]});
+initSidebarItems({"struct":[["Accuracy","The accuracy of the predictions in a ConfusionMatrix."],["ConfusionMatrix","A ConfusionMatrix."],["Sample","A single prediction Sample."]]});
