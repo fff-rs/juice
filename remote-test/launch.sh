7d645d923efa2fe933e3d671be9dd06327fed535
Bernhard Schuster <bernhard@ahoi.io>
2023-02-01 12:38:40 +0100

    fix/ci: concourse based infra now has proper support for seccomp filters

--- remote-test/launch.sh
+++ remote-test/launch.sh
@@ -3,7 +3,6 @@ set -e
 cargo check --tests
 fly -t spearow login -n juice-crashtesters --concourse-url https://ci.spearow.io
 fly -t spearow execute \
-    --tag simsalabim \
     --tag framework:cuda \
     --tag framework:opencl \
     -c ./remote-test/test.yml \
